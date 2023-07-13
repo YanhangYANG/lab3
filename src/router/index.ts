41aacb632986f4326ded39de6247f725cfb9f4cc
yanhang_yang <yanhang_y@cmu.ac.th>
2023-07-13 20:48:26 +0700

    4.2

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,12 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import StudentView from '../views/StudentView.vue'
 import MenuView from '../views/MenuView.vue'
-import EventDetailView from '../views/EventDetailView.vue'
+import EventDetailView from '../views/event/EventDetailView.vue'
+import EventEditView from '@/views/event/EventEditView.vue'
+import EventRegisterView from '@/views/event/EventRegisterView.vue'
+import EventLayoutView from '@/views/event/EventLayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,9 +29,29 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail',
-      component: EventDetailView,
-      props: true
+      name: 'event-layout',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit',
+          component: EventEditView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register',
+          component: EventRegisterView,
+          props: true
+        }
+      ]
     },
     {
       path: '/menu',
@@ -37,6 +62,22 @@ const router = createRouter({
       path: '/student',
       name: 'Student',
       component: StudentView
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error',
+      component: NetworkErrorView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource',
+      component: NotFoundView,
+      props: true
     }
   ]
 })
